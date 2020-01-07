372cfb794cee0c76e044f32e71f9f6728276e81a
Alex23Rodriguez <alex.rodriguez.oro@gmail.com>
2020-01-07 19:47:28 +0100

    before adding global var board size

--- amazons/lib/main.dart
+++ amazons/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math' show min;
 import 'package:flutter/material.dart';
 
 import './board.dart';
@@ -7,21 +8,30 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    boardSize = min(MediaQuery.of(context).size.height.floor(),
+        MediaQuery.of(context).size.width.floor());
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Game of the Amazons"),
         ),
-        body: LayoutBuilder(builder: (context, constraints) {
-          return Container(
-              height: constraints.maxHeight < constraints.maxWidth
-                  ? constraints.maxHeight
-                  : constraints.maxWidth,
-              width: constraints.maxHeight < constraints.maxWidth
-                  ? constraints.maxHeight
-                  : constraints.maxWidth,
-              child: new Board());
-        }),
+        body: Column(
+          children: <Widget>[
+            LayoutBuilder(builder: (context, constraints) {
+              return Container(
+                  height: constraints.maxHeight < constraints.maxWidth
+                      ? constraints.maxHeight
+                      : constraints.maxWidth,
+                  width: constraints.maxHeight < constraints.maxWidth
+                      ? constraints.maxHeight
+                      : constraints.maxWidth,
+                  child: new Board());
+            }),
+            RaisedButton(
+              onPressed: () {},
+            )
+          ],
+        ),
         // floatingActionButton: FloatingActionButton(
         //   onPressed: () {},
         //   child: Icon(Icons.add),
